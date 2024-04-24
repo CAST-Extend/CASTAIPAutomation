9be5a562f84d32eedb256b5fa86067681dde8635
sharath9620 <s.p@castsoftware.com>
2024-04-24 15:09:19 +0530

    bug fixing

--- AIP_Automation.py
+++ AIP_Automation.py
@@ -2,9 +2,9 @@ import os
 import subprocess
 import csv
 import datetime
-import math
 import threading
 import logging
+import re
 
 # Mapping dictionary for return codes and their corresponding messages
 return_code_messages = {
@@ -41,63 +41,71 @@ def validate_config(properties):
         if param not in properties:
             print(f"Program stopped because required parameter '{param}' is not in the config.properties \n")
             raise ValueError(f"Required parameter '{param}' is not in the config.properties")
+        
+def replace_special_characters_with_underscore(input_string):
+    input_string = input_string.replace("^", "").replace("&", "").replace("'","").replace("@","").replace("{","").replace("}","").replace("[","").replace("]","").replace(",","").replace("$","").replace("=","").replace("!","").replace("-","").replace("#","").replace("(","").replace(")","").replace("%","").replace(".","").replace("+","").replace("~","").replace("_","")
+    # Define a regular expression pattern to match spaces and special characters
+    pattern = r'[^a-zA-Z0-9_\s]'
+
+    # Use the sub() function from the re module to replace matches with underscores
+    output_string = re.sub(pattern, '_', input_string)
+
+    return output_string
 
 # Function to process application
 def process_application(app_batch, console_url, console_api_key, console_cli, source_code_path, logger, output_csv_file, output_txt_file):
     try:
-        print(f"Executing Batch: {app_batch} \n")
-        logger.info(f"Executing Batch: {app_batch}")
-        for app_name, app_domain in app_batch:
+        # print(f"Executing Batch: {app_batch} \n")
+        # logger.info(f"Executing Batch: {app_batch}")
+        for application_name, app_domain in app_batch:
+
+            app_name = replace_special_characters_with_underscore(application_name)
+
+            print(f"Application -> '{application_name}' is renamed as '{app_name}' \n")
+            logger.info(f"Application -> '{application_name}' is renamed as '{app_name}' \n")
+
+            print(f"Running AIP Analysis for the application -> '{app_name}' ..... \n")
+            logger.info(f"Running AIP Analysis for the application -> '{app_name}' ..... \n")
+
             command = [
                 'java', '-jar', f'{console_cli}',
                 'add',
                 '-n', f'{app_name}',
                 '--domain-name', f'{app_domain}',
-                '-f', f'{source_code_path}/{app_name}',
+                '-f', f'{source_code_path}/{application_name}',
                 '-s',  f'{console_url}',
                 '--apikey', f'{console_api_key}',
                 '--verbose',
                 '--auto-create',
+                '--process-imaging',
                 'Publish-Imaging',
                 '--upload-application=true',
                 '--exclude-patterns="tmp/, temp/, *test, tests, target/, .svn/, .git/, _Macosx/, test/"'
             ]
-            print(f"Executing command for Application-{app_name} to run AIP Analysis:", " ".join(command), "\n")
+            cmd = " ".join(command)
+            print(f"Executing command to run AIP Analysis for the Application -> '{app_name}' : {cmd} \n")
+            logger.info(f"Executing command to run AIP Analysis for the Application -> '{app_name}' : {cmd} \n")
             # logger.info("Executing command:", " ".join(command))
 
             completed_process = subprocess.run(command, capture_output=True, text=True)
             return_code = completed_process.returncode
             if return_code == 0:
-                command_2 = [
-                    'java', '-jar', f'{console_cli}',
-                    'Publish-Imaging',
-                    '-n', f'{app_name}',
-                    '-s',  f'{console_url}',
-                    '--apikey', f'{console_api_key}',
-                    '--verbose'
-                ]
-                print(f"Executing command for Application-{app_name} for Imaging upload:", " ".join(command_2), "\n")
-                # logger.info("Executing command:", " ".join(command))
+                status = "Passed"
+                reason = "Application processed successfully"
+                print(f"Application -> '{app_name}' processed successfully \n")
+                logger.info(f"Application -> '{app_name}' processed successfully \n")
 
-                completed_process_2 = subprocess.run(command_2, capture_output=True, text=True)
-                return_code_2 = completed_process_2.returncode
-
-                if return_code_2 == 0:
-                    status = "Passed"
-                    reason = "Application processed successfully"
-                    print(f'Application - {app_name} processed successfully\n')
-                    logger.info(f'Application - {app_name} processed successfully\n')
-                else:
-                    status = "Failed"
-                    reason = return_code_messages.get(return_code, f"Unknown return code: {return_code}")
-                    print(f'Application - {app_name} Failed with Unknown return code: {return_code}\n')
-                    logger.info(f'Application - {app_name} Failed with Unknown return code: {return_code}\n') 
+            elif return_code in return_code_messages.keys():
+                status = "Failed"
+                reason = return_code_messages.get(return_code, f"known return code: {return_code} - {return_code_messages[return_code]}")
+                print(f"Application -> '{app_name}' Failed with known return code: {return_code} - {return_code_messages[return_code]} \n")
+                logger.info(f"Application -> '{app_name}' Failed with known return code: {return_code} - {return_code_messages[return_code]} \n")   
 
             else:
                 status = "Failed"
                 reason = return_code_messages.get(return_code, f"Unknown return code: {return_code}")
-                print(f'Application - {app_name} Failed with Unknown return code: {return_code}\n')
-                logger.info(f'Application - {app_name} Failed with Unknown return code: {return_code}\n')                
+                print(f"Application -> '{app_name}' Failed with Unknown return code: {return_code} \n")
+                logger.info(f"Application -> '{app_name}' Failed with Unknown return code: {return_code} \n")               
 
             # Write output data to CSV
             with open(output_csv_file, 'a', newline='') as csvfile:
@@ -109,13 +117,21 @@ def process_application(app_batch, console_url, console_api_key, console_cli, so
                 txtfile.write(f"{app_name}: {status} - {reason}\n")
 
     except Exception as e:
-        print("Error processing application:", e, "\n")
-        logger.error("Error processing application:", e)
+        print(f"Error processing application -> '{app_name}' : {e} \n")
+        logger.error(f"Error processing application -> '{app_name}' : {e} \n")
 
 # Function to create batches
-def create_batches(applications, max_batches):
-    batch_size = math.ceil(len(applications) / max_batches)
-    batches = [applications[i * batch_size:(i + 1) * batch_size] for i in range(max_batches)]
+def create_batches(applications, num_batches):
+    batch_size = len(applications) // num_batches
+    remaining_apps = len(applications) % num_batches
+    batches = []
+    start = 0
+    
+    for i in range(num_batches):
+        batch_end = start + batch_size + (1 if i < remaining_apps else 0)
+        batches.append(applications[start:batch_end])
+        start = batch_end
+    
     return batches
 
 # Function to process batch
@@ -180,13 +196,21 @@ def main():
 
         # Create batches
         batches = create_batches(applications, max_batches)
+        num_of_apps = len(applications)
+        num_of_batches = len(batches)
+        print(f"{num_of_apps} Applications divided into {num_of_batches} Batches.\n")
+        logger.info(f"{num_of_apps} Applications divided into {num_of_batches} Batches.\n")
+
+        for count,app_batch in enumerate(batches, start=1):
+            print(f"Batch-{count} {app_batch}\n")
+            logger.info(f"Batch-{count} {app_batch}\n")
 
         # Process batches
         process_batch(batches, console_url, console_api_key, console_cli, source_code_path, logger, output_csv_file, output_txt_file)
 
     except Exception as e:
         print("Error:", e, "\n")
-        logger.error("Error:", e)
+        logger.error("Error:", e, "\n")
 
 if __name__ == "__main__":
     main()
